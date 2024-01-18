8646b69cf010a0e404805c6635a7bb42e9eac26b
Oscar Byiringiro <byiringoroscar@gmail.com>
2024-01-18 15:14:57 +0200

    Creating model

--- api/models.py
+++ api/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Information(models.Model):
+    country = models.CharField(max_length=250)
+    flag = models.URLField()
+    csirt = models.CharField(max_length=250, default='None', null=True, blank=True)
+    web = models.URLField(default='None', null=True, blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        verbose_name_plural = 'Information'
+
+    def __str__(self):
+        return self.country
