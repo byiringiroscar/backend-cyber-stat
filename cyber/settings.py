a344f9de9e22120f2d216ca89483cad07b762bca
Oscar Byiringiro <byiringoroscar@gmail.com>
2024-01-19 19:27:29 +0200

    Update settings.py

--- cyber/settings.py
+++ cyber/settings.py
@@ -121,7 +121,7 @@ USE_TZ = True
 CORS_ALLOWED_ORIGINS = [
     "http://127.0.0.1:3000",
     "https://cyberspector.vercel.app",
-    "https://cyberspector.onrender.com"
+    "https://cyberspector.onrender.com",
 ]
 
 
