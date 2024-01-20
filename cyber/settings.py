ca94394dca311009d3d530a714b068d2a7b92c1f
Oscar Byiringiro <byiringoroscar@gmail.com>
2024-01-21 01:33:06 +0200

    Update settings.py

--- cyber/settings.py
+++ cyber/settings.py
@@ -124,14 +124,14 @@ USE_I18N = True
 
 USE_TZ = True
 
-CORS_ALLOWED_ORIGINS = [
-    "https://example.com",
-    "https://sub.example.com",
-    "http://localhost:8080",
-    "http://127.0.0.1:3000",
-]
+# CORS_ALLOWED_ORIGINS = [
+#     "https://example.com",
+#     "https://sub.example.com",
+#     "http://localhost:8080",
+#     "http://127.0.0.1:3000",
+# ]
 
-# CORS_ALLOW_ALL_ORIGINS = True
+CORS_ALLOW_ALL_ORIGINS = True
 
 
 
