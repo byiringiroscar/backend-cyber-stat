a5aa75493a6a71724b7399e201d4eb615fd71dad
Oscar Byiringiro <byiringoroscar@gmail.com>
2024-01-21 00:24:04 +0200

    wip

--- cyber/settings.py
+++ cyber/settings.py
@@ -118,13 +118,14 @@ USE_I18N = True
 
 USE_TZ = True
 
-CORS_ALLOWED_ORIGINS = [
-    "http://127.0.0.1:3000",
-    "https://cyberspector.vercel.app",
-    "https://cyberspector.onrender.com",
-]
+# CORS_ALLOWED_ORIGINS = [
+#     "http://127.0.0.1:3000",
+#     "https://cyberspector.vercel.app",
+#     "https://cyberspector.onrender.com",
+# ]
 
-CORS_ALLOW_ALL_ORIGINS: True
+CORS_ALLOW_ALL_ORIGINS = True
+
 
 
 # Static files (CSS, JavaScript, Images)
