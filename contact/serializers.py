fa48b7b9b35aa8e34c2a4ba07f55154a011acad4
Oscar Byiringiro <byiringoroscar@gmail.com>
2024-01-21 12:15:47 +0200

    wip fix endpoint

--- contact/serializers.py
+++ contact/serializers.py
@@ -5,4 +5,4 @@ from .models import Contact
 class ContactSerializer(serializers.ModelSerializer):
     class Meta:
         model = Contact
-        fields = ['name', 'message', 'email']
+        fields = ['id', 'name', 'message', 'email']
