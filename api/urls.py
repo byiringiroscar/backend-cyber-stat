50c7a7ec2b8847d08cd3552a589267b10e331b82
Oscar Byiringiro <byiringoroscar@gmail.com>
2024-01-23 15:42:08 +0200

    Get country endpoint

--- api/urls.py
+++ api/urls.py
@@ -4,4 +4,5 @@ from .views import *
 urlpatterns = [
     path('information/', InformationListCreateAPIView.as_view(), name='information-list-create'),
     path('information/<str:id>/', InformationRetrieveUpdateDeleteAPIView.as_view(), name='information-detail'),
+    path('information/country/<str:country>/', get_country, name='country-detail'),
 ]
--- api/views.py
+++ api/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
 from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
 from .serializer import InformationSerializer
+from rest_framework import status
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from .models import *
 
 
 # Create your views here.
@@ -15,3 +19,16 @@ class InformationRetrieveUpdateDeleteAPIView(RetrieveUpdateDestroyAPIView):
     serializer_class = InformationSerializer
     queryset = InformationSerializer.Meta.model.objects.all()
     lookup_field = 'id'
+
+
+@api_view(['GET', ])
+def get_country(request, country):
+    country = str(country)
+    try:
+        country = Information.objects.filter(country=country)
+    except InformationSerializer.Meta.model.DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+
+    if request.method == 'GET':
+        serializer = InformationSerializer(country, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
